60344cc988918fe95f6d264856e4c71595470ba1
Jon Struve <ocdammo3@gmail.com>
2019-11-14 15:57:05 -0600

    Have information appearing for each characted by their character id

--- start/routes.js
+++ start/routes.js
@@ -18,8 +18,6 @@ const superagent = require('superagent');
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route');
 
-const swapiRequest = 'https://swapi.co/api/people/1';
-
 function getInformation(url) {
   console.log(`it's running friend`);
   return superagent.get(url)
@@ -33,8 +31,8 @@ function getInformation(url) {
 }
 
 Route.on('/').render('welcome', { username: 'Steven' });
-Route.get('/star-wars-card', ({ view }) => {
-  return getInformation(swapiRequest) // returns Promise that resolves with data
+Route.get('/star-wars-card/:id', ({ params, view }) => {
+  return getInformation(`https://swapi.co/api/people/${params.id}`) // returns Promise that resolves with data
     .then(data => {
       return view.render('star-wars-card', data)
     })
@@ -44,6 +42,29 @@ class Person {
   constructor(data) {
     this.name = data.name,
     this.height = data.height,
-    this.homeworld = data.homeworld
+    this.homeworld = data.homeworld,
+    this.gender = data.gender,
+    this.pronoun = this.pronounCheck(),
+    this.pastTense = this.pastPronoun()
+  }
+
+  pronounCheck() {
+    if(this.gender === 'male') {
+      return 'He';
+    } else if(this.gender === 'female') {
+      return 'She';
+    } else if(this.gender === 'n/a') {
+      return 'They';
+    }
+  }
+
+  pastPronoun() {
+    if(this.gender === 'male') {
+      return 'his';
+    } else if(this.gender === 'female') {
+      return 'her';
+    } else if(this.gender === 'n/a') {
+      return 'their';
+    }
   }
 }
